60c4e42c68f15247140fc70ba08cae8f8086245b
Niklas Grewe <niklas@grewe.io>
2021-03-11 12:16:42 +0100

    install dependencies

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,20 @@
 <script>
-    import Test from '$components/Test.svelte';
+    import { useForm, minLength, email, required } from 'svelte-use-form'
+
+    const form = useForm({
+        email: {
+            validators: [required, email],
+            errorMap: { required: "Please enter an email address", email: "This is not a valid email address" }
+        },
+        password: {
+            validators: [required, minLength(6)],
+            errorMap: { required: "Please enter your password", minLength: value => `The Password requires at least ${value} characters` }
+        }
+    })
 </script>
 
-<style lang="postcss">
-    h1 {
-        @apply text-5xl text-gray-900 font-semibold;
-    }
-</style>
+<form use:form>
+    <input type="email">
+    <input type="password">
+</form>
 
-<h1>Hello Svelte here!</h1>
-
-<Test />
